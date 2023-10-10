1a06ff32ec9a45cc74ff6bb9b015f86c272dd458
Jordi Manyà <2023_jordi.manya@iticbcn.cat>
2023-10-10 19:55:29 +0200

    pràctica PHP Jordi

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # M07_login
+
+**Jordi Manyà**
+
+- Base de dades creada: 
+![alt](capturaBbdd.png "Base de dades creada")
+
+- Missatge d'execució correcta després d'omplir el formulari:
+![Alt text](delocs.png "Tot bé")
+
+- Base de dades després de l'INSERT:
+![alt](captura2.png "Base de dades després de l'INSERT")
---
+++ basedados.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Base de dades</title>
+</head>
+
+<body>
+    <?php
+    // CONSTANTS DE LA CONNEXIÓ A LA BASE DE DADES
+    define("DB_HOST", "localhost");
+    define("DB_NAME", "Users");
+    define("DB_USER", "root");
+    define("DB_PSW", "");
+
+    // CONNEXIÓ A LA BBDD
+    $connexio = mysqli_connect(DB_HOST, DB_USER, DB_PSW, DB_NAME);
+
+    var_dump($connexio);
+
+    // VARIABLES PER REBRE ELS VALORS
+    $user_id = $_POST['user_id'];
+    $name = $_POST['nom'];
+    $surname = $_POST['cognom'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+    $rol = $_POST['rol'];
+    $active = $_POST['active'];
+
+    // CONDICIONAL PER SABER SI LA CONNEXIÓ ÉS CORRECTA
+    if (!$connexio) {
+        echo "Error de connexió " . mysqli_connect_error();
+    }
+
+    $query = "INSERT INTO user (user_id, name, surname, password, email, rol, active) VALUES ($user_id, $name, $surname, $password, $email, $rol, $active)";
+    if ($user = mysqli_query($connexio, $query)) {
+        header('Location: resultat.php');
+    } else {
+        echo "ERROR: " . $user . "<br>" . mysqli_error($connexio);
+    }
+    mysqli_close($connexio);
+
+    ?>
+</body>
+
+</html>
